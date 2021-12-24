382114f033751c32f9f3038156188228a34cf467
vijayshankarrealdeal <vijaygaya056@gmail.com>
2021-12-24 19:00:56 +0530

    hhhgf

--- lib/routes/food_pre_post.dart
+++ lib/routes/food_pre_post.dart
@@ -1,13 +1,10 @@
 import 'package:airplane/controllers/colormager.dart';
 import 'package:airplane/controllers/movie_controllers.dart';
 import 'package:airplane/controllers/typography.dart';
-import 'package:airplane/model/shop_model.dart';
 import 'package:airplane/widgets/loading_spinner.dart';
 import 'package:airplane/widgets/show_items_tab.dart';
-import 'package:auto_size_text/auto_size_text.dart';
-import 'package:cached_network_image/cached_network_image.dart';
+
 import 'package:flutter/material.dart';
-import 'package:google_fonts/google_fonts.dart';
 import 'package:provider/provider.dart';
 
 class FoodPrePost extends StatelessWidget {
@@ -31,8 +28,8 @@ class FoodPrePost extends StatelessWidget {
           backgroundColor: color.appBarColorroute(),
           bottom: TabBar(
             tabs: [
-              fonts.body1("Pre", color.textColor()),
-              fonts.body1("Post", color.textColor()),
+              fonts.heading6("Pre-Security", color.textColor()),
+              fonts.heading6("Post-Security", color.textColor()),
             ],
           ),
         ),
